9572eb0f0867d5311284115e19dc8fa611cdcf1c
marcali <alina_marchenko@outlook.com>
2024-03-19 16:48:40 +0000

    updated .py files

--- src/task1/task.py
+++ src/task1/task.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 import  torch
+import time
 from torch.utils.data import TensorDataset, DataLoader
-torch.manual_seed(0)
+torch.manual_seed(123)
 
 
 def polynomial_fun(w, x):
@@ -11,9 +12,9 @@ def polynomial_fun(w, x):
     powers_of_x = x**torch.arange(len(w)).view(1, -1)
     y = torch.mm(powers_of_x, w)
 
-    return y
+    return y.squeeze()
 
-def fit_polynomial_ls(x, t, M = 0):
+def fit_polynomial_ls(x, t, M):
     x = x.view(-1, 1)
     t = t.view(-1, 1)
 
@@ -23,96 +24,176 @@ def fit_polynomial_ls(x, t, M = 0):
 
     return w.view(-1)
 
-def fit_polynomial_sgd(x, t, M = 0, lr=1e-2, miniBatchSize=5, print_freq=100, N_epochs=500):
+def fit_polynomial_sgd(x, t, M = 0, lr=1e-2, miniBatchSize=5, print_freq=100, N_epochs=2000):
+    #initialise weights to 1
+    torch.manual_seed(123)
+
     x = x.view(-1, 1)
     t = t.view(-1, 1)
-    powers_of_x = x**torch.arange(M + 1).view(1, -1)
-    
-    model = torch.nn.Linear(M + 1, 1, bias=False)
-    criterion = torch.nn.MSELoss()
-    #adagrad
-    opt = torch.optim.Adagrad(model.parameters(), lr=0.1)
-    dataset = TensorDataset(powers_of_x, t)
+    pow_of_x = x**torch.arange(M + 1).view(1, -1)
+    weights = torch.rand(M + 1, 1, requires_grad=True)
+
+    #print('weights ', model.weight.data)
+    #change to sgd
+    opt = torch.optim.Adam([weights], lr = lr)
+    dataset = TensorDataset(pow_of_x, t)
     loader = DataLoader(dataset, batch_size=miniBatchSize, shuffle=True)
+    mse_loss =  torch.nn.MSELoss()
+    total_loss = 0.0
+    num_batches = 0
+    min_loss = torch.inf
+    loss_diff_threshold = 1e-3
+    losses = []
+    grad_norms = []
+    print('Training with SGD , polynomial degree ', M)
     
     for epoch in range(N_epochs):
         for batch_x, batch_t in loader:
+            #normalize batch x
+            #batch_x = (batch_x - batch_x.mean())/batch_x.std()
+            #check if this is supposed to be after step
             opt.zero_grad()
-            batch_y = model(batch_x)
+            #used wights multiplication wirg batch x to do the prediction
+            batch_y = batch_x@weights
+            #print('output',  batch_y)
             #mean square loss
-            loss = torch.nn.MSELoss(batch_y, batch_t)
-            loss.backward()
+            loss = mse_loss(batch_y, batch_t)
+            loss.backward() 
+            #TODO: for plotting only
+            grad_norms.append(weights.grad.norm().item())
+
+            #torch.nn.utils.clip_grad_value_(weights, 1)
+            total_loss += loss.item()
+            num_batches += 1
             opt.step()
-            if (epoch + 1) % print_freq == 0:
-                print('epoch {} loss {}'.format(epoch+1, loss.item()))
+
+        avg_loss = total_loss / num_batches
+        losses.append(avg_loss)
+        
+        #doesn't d much 
+        if abs(min_loss - loss.item()) < loss_diff_threshold:
+            print('Early stopping at epoch {} with loss {}'.format(epoch+1, avg_loss))
+            break;
+        min_loss = avg_loss
+        
+        if (epoch + 1) % print_freq == 0:
+            print('epoch {} loss {}'.format(epoch+1, avg_loss))
+        total_loss = 0.0
+        num_batches = 0
+        
+    #Plotting
+    #TODO:remove
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(losses)
+    plt.title('Loss per epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(grad_norms)
+    plt.title('Gradient norm per epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Gradient norm')
+    plt.show()
                 
-    return model.weight.data.view(-1)
+    return weights.detach()
 
 #implement task script 1
+if __name__ == '__main__':
+    #1) Use polynomial_fun (𝑀 = 2, 𝐰 = [1,2,3]T) to generate a training set and a test set
+    w = torch.tensor([1, 2, 3], dtype=torch.float32).unsqueeze(1)
+    x_train = torch.linspace(-20, 20, 20)
+    x_test = torch.linspace(-20, 20, 10)
+    y_train = polynomial_fun(w, x_train)
+    y_test = polynomial_fun(w, x_test)
+    #observed t values with gauss noise 
+    t_train = y_train + 0.5 * torch.randn_like(y_train)
+    t_test = y_test + 0.5 * torch.randn_like(y_test)
+    M = torch.tensor([2,3,4])
 
-#1) Use polynomial_fun (𝑀 = 2, 𝐰 = [1,2,3]T) to generate a training set and a test set
-w = torch.tensor([1, 2, 3], dtype=torch.float32).unsqueeze(1)
-x_train = torch.linspace(-20, 20, 20)
-x_test = torch.linspace(-20, 20, 10)
-y_train = polynomial_fun(w, x_train)
-y_test = polynomial_fun(w, x_test)
-#observed t values with gauss noise 
-t_train = y_train + 0.5 * torch.randn_like(y_train)
-t_test = y_test + 0.5 * torch.randn_like(y_test)
-
-#2) Use fit_polynomial_ls (𝑀𝜖{2,3,4}) to compute the optimum weight 
-
-M = torch.tensor([2,3,4])
-
-#For each 𝑀, compute the predicted target values 𝑦̂ for all 𝑥 in both the training and test sets.
-pred_train = torch.zeros(len(x_train), len(M))
-pred_test = torch.zeros(len(x_test), len(M))
-for i, m in enumerate(M):
-    w_hat = fit_polynomial_ls(x_train,t_train,m.item()).unsqueeze(1)
-    pred_train[:, i] = polynomial_fun(w_hat, x_train).squeeze()
-    pred_test[:, i] = polynomial_fun(w_hat, x_test).squeeze()
-    
-# Report, using printed messages, the mean (and standard deviation) in difference a) between
-# the observed training data and the underlying “true” polynomial curve; and b) between the
-# “LS-predicted” values and the underlying “true” polynomial curve.
-#both test and train
-
-diff_obs_train = t_train - y_train
-diff_pred_train = pred_train - y_train
-diff_obs_test = t_test - y_test
-diff_pred_test = pred_test - y_test
-
-sd_obs_train, mean_obs_train = torch.std_mean(diff_obs_train)
-sd_pred_train, mean_pred_train = torch.std_mean(diff_pred_train)
-sd_obs_test, mean_obs_test = torch.std_mean(diff_obs_test)
-sd_pred_test, mean_pred_test = torch.std_mean(diff_pred_test)
+    # diff in noisy and not noisy data 
+    diff_obs_train = t_train - y_train
+    diff_obs_test = t_test - y_test
+    #mean diff
+    sd_obs_train, mean_obs_train = torch.std_mean(diff_obs_train)
+    sd_obs_test, mean_obs_test = torch.std_mean(diff_obs_test)
+    #rmse
+    rmse_obs_train = torch.sqrt(torch.mean(torch.square(diff_obs_train)))
+    rmse_obs_test = torch.sqrt(torch.mean(torch.square(diff_obs_test)))
+    print('Training RMSE between the observed training data and the underlying “true” polynomial curve is ', rmse_obs_train.item())
+    print('Test RMSE between the observed training data and the underlying “true” polynomial curve in training data is ', rmse_obs_test.item())
+    print('Test Mean difference between the observed training data and the underlying “true” polynomial curve in training data is ', mean_obs_test.item(), 'and standard diviation is', sd_obs_test.item())
+    print('Training Mean difference between the observed training data and the underlying “true” polynomial curve is ', mean_obs_train.item(), 'and standard diviation is', sd_obs_train.item() )
 
 
-print('Training Mean difference between the observed training data and the underlying “true” polynomial curve is ', mean_obs_train.item(), 'and standard diviation is', sd_obs_train.item() )
-print('Training Mean difference between “LS-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_train.item(), sd_pred_train.item()) )
-print('Test Mean difference between the observed training data and the underlying “true” polynomial curve in training data is ', mean_obs_test.item(), 'and standard diviation is', sd_obs_test.item())
-print('Test  Mean difference between “LS-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_test.item(), sd_pred_test.item()) )
 
-    
-#Use fit_polynomial_sgd (𝑀𝜖{2,3,4}) to optimise the weight vector 𝐰̂ using the training set.
-# For each 𝑀, compute the predicted target values 𝑦̂ for all 𝑥 in both the training and test sets.
-#  Report, using printed messages, the mean (and standard deviation) in difference between the
-# “SGD-predicted” values and the underlying “true” polynomial curve
-pred_train_opt = torch.zeros(len(x_train), len(M))
-pred_test_opt = torch.zeros(len(x_test), len(M))
-for i, m in enumerate(M):
-    w_hat_opimized = fit_polynomial_sgd(x_train,t_train,m.item()).unsqueeze(1)
-    pred_train_opt[:, i] = polynomial_fun(w_hat, x_train).squeeze()
-    pred_test_opt[:, i] = polynomial_fun(w_hat, x_test).squeeze()
+    for i, m in enumerate(M):
+        if len(w) < m.item() + 1:
+            w = torch.cat((w, w[-1].unsqueeze(0) + 1))
+        #fitting LS
+        print('For polynomial degree ', m.item())
+        t0 = time.time()
+        w_hat = fit_polynomial_ls(x_train, t_train, m.item()).unsqueeze(1)
+        t1 = time.time()
+        print("elapsed time for fit_polynomial_ls: %.2f seconds" % (t1-t0))
 
-diff_pred_train_opt = pred_train_opt - y_train
-diff_pred_test_opt = pred_test_opt - y_test
+        pred_train = polynomial_fun(w_hat, x_train).squeeze()
+        pred_test = polynomial_fun(w_hat, x_test).squeeze()
+        
+        #rmse
+        diff_pred_train = pred_train - t_train
+        diff_pred_test = pred_test - t_test
+        diff_pred_weights = w.view(-1, 1) - w_hat
+        
+        rmse_train = torch.sqrt(torch.mean(torch.square(diff_pred_train)))
+        rmse_test = torch.sqrt(torch.mean(torch.square(diff_pred_test)))
+        rmse_weights = torch.sqrt(torch.mean(torch.square(diff_pred_weights)))
+        std_dev_train = torch.std(diff_pred_train)
+        std_dev_test = torch.std(diff_pred_test)
+        
+        #mean diff
+        sd_pred_train, mean_pred_train = torch.std_mean(diff_pred_train)
+        sd_pred_test, mean_pred_test = torch.std_mean(diff_pred_test)
 
-sd_pred_train_opt, mean_pred_train_opt = torch.std_mean(diff_pred_train_opt)
-sd_pred_test_opt, mean_pred_test_opt = torch.std_mean(diff_pred_test_opt)
+        print('Training Mean difference between “LS-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_train.item(), sd_pred_train.item()) )
+        print('Test  Mean difference between “LS-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_test.item(), sd_pred_test.item()) )
+        print('Training RMSE between the LS values and the underlying “true” polynomial curve is {:.4f}, and standard deviation is {:.4f}'.format( rmse_train.item(), std_dev_train.item()))
+        print('Test RMSE between the LS values and the underlying “true” polynomial curve is {:.4f}, and standard deviation is {:.4f}'.format( rmse_test.item(), std_dev_test.item()))
+        print('RMSE between the true weights and predicted by LS weights is {:.3f}'.format( rmse_weights.item()))
 
-print('Training Mean difference between “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_train_opt.item(), sd_pred_train_opt.item()) )
-print('Test  Mean difference between “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard diviation: {:.3f}' .format(mean_pred_test_opt.item(), sd_pred_test_opt.item()) )
-
-#using sgd, approximate your M using continous number of variables, to optimize M
-#consider how you are updating your optimized, if it's not optimum what can u do about it 
+        #for SGD
+        t0 = time.time()
+        w_hat_opimized = fit_polynomial_sgd(x_train, t_train, m.item())
+        t1 = time.time()
+        print("elapsed time for fit_polynomial_sgd: %.2f seconds" % (t1-t0))
+        #training
+        pred_train_opt = polynomial_fun(w_hat_opimized, x_train).squeeze()
+        
+        #test
+        pred_test_opt = polynomial_fun(w_hat_opimized, x_test).squeeze()
+        
+        #mean difference 
+        std_difference_tr, mean_difference_tr = torch.std_mean(torch.abs(pred_train_opt - y_train))
+        std_difference_te, mean_difference_te = torch.std_mean(torch.abs(pred_train_opt - y_train))
+        print('Training Mean absolute difference between the “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard deviation is {:.3f}'.format( mean_difference_tr.item(), std_difference_tr.item()))
+        print('Test Mean absolute difference between the “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard deviation is {:.3f}'.format( mean_difference_te.item(), std_difference_te.item()))
+        
+        #rmse
+        diff_pred_train_opt = pred_train_opt - t_train
+        diff_pred_test_opt = pred_test_opt - t_test
+        diff_pred_weights_opt = w.view(-1, 1) - w_hat_opimized
+        rmse_train_opt= torch.sqrt(torch.mean(torch.square(diff_pred_train_opt)))
+        rmse_test_opt= torch.sqrt(torch.mean(torch.square(diff_pred_test_opt)))
+        rmse_weights_opt = torch.sqrt(torch.mean(torch.square(diff_pred_weights_opt)))
+        std_dev_train_opt = torch.std(diff_pred_train_opt)
+        std_dev_test_opt = torch.std(diff_pred_train_opt)
+        print('Training RMSE  between the “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard deviation is {:.3f}'.format( rmse_train_opt.item(), std_dev_train_opt.item()))
+        print('Test RMSE between the “SGD-predicted” values and the underlying “true” polynomial curve is {:.3f}, and standard deviation is {:.3f}'.format( rmse_test_opt.item(), std_dev_test_opt.item()))
+        print('RMSE between the true weights and predicted by SGD weights is {:.3f}'.format( rmse_weights_opt.item()))
+        
+            #TODO: remove all plots
+        fig, ax = plt.subplots()
+        ax.plot(x_train, y_train, 'o')
+        ax.plot(x_train, pred_train_opt, 'v')
--- src/task2/task.py
+++ src/task2/task.py
@@ -8,19 +8,164 @@ from PIL import Image
 import ssl
 from torch.cuda.amp import autocast, GradScaler
 from torchvision.models import VisionTransformer
+from matplotlib import pyplot as plt
+import numpy as np
+from torch.nn.functional import one_hot
+torch.manual_seed(123)
+torch.cuda.manual_seed_all(123)
+
+
+class MixUp:
+    def __init__(self, num_classes, alpha=0.2, method=1):
+        self.alpha = alpha
+        self.method = method
+        self.num_classes = num_classes
+
+    def __call__(self, images, labels):
+        labels = one_hot(labels, self.num_classes)
+        
+        if self.method == 1:
+            lambd = torch.distributions.Beta(self.alpha, self.alpha).sample((images.size(0),)).to(images.device)
+        elif self.method == 2:
+            lambd = torch.distributions.Uniform(0.1, 0.4).sample((images.size(0),)).to(images.device)
+
+        #mixing random images
+        indices = torch.randperm(images.size(0))
+
+        images1, labels1 = images, labels
+        images2, labels2 = images[indices], labels[indices]
+
+        mix_images = lambd.view(-1, 1, 1, 1) * images1 + (1 - lambd).view(-1, 1, 1, 1) * images2
+        mix_labels = lambd.view(-1, 1) * labels1 + (1 - lambd).view(-1, 1) * labels2
+
+        return mix_images, mix_labels
+    
+def imshow(img):
+    img = img / 2 + 0.5  # Unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.axis('off')
+    
+def train_and_evaluate(trainloader, testloader, num_epochs, save_filename, sampling_method):
+    ## vision transformer 
+    net = VisionTransformer(image_size=32, patch_size=8, num_layers=6, num_heads=8,
+                            hidden_dim=384, mlp_dim=1536, dropout=0.0, num_classes=len(classes)).cuda()
+
+    ## loss and optimiser
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    mixUp = MixUp(num_classes=len(classes),alpha=2,method=sampling_method)
+    results_train = []
+    results_test = []
+    ## train
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        correct_train = 0
+        total_train = 0
+        running_loss = 0.0
+        print('Epoch {}, sampling method {}'.format(epoch+1, sampling_method))
+        net.train()
+        for i, data in enumerate(trainloader, 0):
+
+            images, labels = data
+            images, labels = mixUp(images, labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            with autocast():
+                outputs = net(images.cuda())
+                #compute accuracy
+                _, predicted = torch.max(outputs.data, 1)
+                total_train += labels.size(0)
+                # Convert one-hot encoded labels to class indices
+                labels_indices = labels.argmax(dim=1).cuda()
+
+                correct_train += (predicted == labels_indices).sum().item()
+                loss = criterion(outputs, labels_indices)
+            
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)  # Call scaler.step() instead of optimizer.step()
+            scaler.update()  # Update the scaler
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 200 == 199:    # print every 200 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+                
+        print('Training done.')
+        train_accuracy = 100 * correct_train / total_train
+        print('Epoch {}, Training accuracy: {}%'.format(epoch+1, train_accuracy))
+        results_train.append(train_accuracy)        
+        # evaluation on test
+        net.eval()
+        correct_test = 0
+        total_test = 0
+
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images.cuda())
+                _, predicted = torch.max(outputs.data, 1)
+                total_test += labels.size(0)
+                correct_test += (predicted == labels.cuda()).sum().item()
+
+        test_accuracy = 100 * correct_test / total_test
+        print('Epoch {}, Testing accuracy: {}%'.format(epoch+1, test_accuracy))
+        results_test.append(test_accuracy)    # save trained model
+    
+    # After training, plot the accuracies
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(1, num_epochs+1), results_train, label='Train')
+    plt.plot(range(1, num_epochs+1), results_test, label='Test')
+    plt.title('Accuracy vs. Epoch sampling method '+ str(sampling_method) )
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('Accuracy_vs_Epoch_metod_'+ str(sampling_method)+ '.png')
+    plt.show()
+    
+    # Get some random test images
+    #TODO: make sure this generates 36 images
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+
+    # Get predictions for these images
+    outputs = net(images.cuda())
+    _, predicted = torch.max(outputs.data, 1)
+
+    # Prepare the figure
+    fig = plt.figure(figsize=(10, 10))
+
+    # For each image in the batch
+    for i in range(batch_size):
+        ax = fig.add_subplot(6, 6, i+1, xticks=[], yticks=[])
+        imshow(images[i])
+        ax.set_title(f"GT:{classes[labels[i]]}\nPred:{classes[predicted[i]]}", color=("green" if predicted[i]==labels[i] else "red"))
+
+    # Save the figure
+    plt.savefig(f"result_{sampling_method}.png")
+    print('result.png saved.')
+    
+    torch.save(net.state_dict(), save_filename)
+    print(f"Model {sampling_method} saved.")
 
 
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == '__main__':
+
     scaler = GradScaler()
     ## cifar-10 dataset
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    batch_size = 16
+    batch_size = 36
 
     #training set
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
@@ -36,82 +181,20 @@ if __name__ == '__main__':
     dataiter = iter(trainloader)
     images, labels = next(dataiter) # note: for pytorch versions (<1.14) use dataiter.next()
 
-    #TODO:
-    #mix up
+    #TODO: make sure this generates 16 images
+    #mix up 
+    mixup = MixUp(len(classes), 2)
+    images, labels = mixup(images, labels)
 
     #save images
-    im = Image.fromarray((torch.cat(images.split(1,0),3).squeeze()/2*255+.5*255).permute(1,2,0).numpy().astype('uint8'))
+    im = Image.fromarray((torch.cat(images[:16].split(1,0),3).squeeze()/2*255+.5*255).permute(1,2,0).numpy().astype('uint8'))
     im.save("mixup.jpg")
     print('mixup.jpg saved.')
-    print('Ground truth labels:' + ' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
-
-
-    #training
-    ## vision transformer 
-    net = VisionTransformer(image_size=32, patch_size=2, num_layers=6, num_heads=16,
-                            hidden_dim=512, mlp_dim=2048, num_classes=len(classes)).cuda()
-
-    ## loss and optimiser
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    results_train = []
-    results_test = []
-
-    ## train
-    for epoch in range(20):  # loop over the dataset multiple times
-        correct_train = 0
-        total_train = 0
-        running_loss = 0.0
-        net.train()
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            #TODO: maybe romove autocast for the final one
-            with autocast():
-                outputs = net(inputs.cuda())
-                #compute accuracy
-                _, predicted = torch.max(outputs.data, 1)
-                total_train += labels.size(0)
-                correct_train += (predicted == labels.cuda()).sum().item()
-                loss = criterion(outputs, labels.cuda())
-            
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)  # Call scaler.step() instead of optimizer.step()
-            scaler.update()  # Update the scal
-            # loss.backward()
-            # optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i % 200 == 199:    # print every 200 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-                
-            accuracy = 100 * correct_train / total_train
-        print('Training done.')
-        results_train.append(accuracy)
-        print('Accuracy of the network on the train images: {}%'.format(accuracy))
+    class_indices = labels.argmax(dim=1)
+    print('Ground truth labels:' + ' '.join('%5s' % classes[class_indices[j]] for j in range(batch_size)))
         
-        # evaluation on test
-        net.eval()
-        correct_test = 0
-        total_test = 0
+    #do smapling method 1
+    train_and_evaluate(trainloader, testloader, 1, 'saved_model_sampling_method1.pt', 1)
 
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                outputs = net(images.cuda())
-                _, predicted = torch.max(outputs.data, 1)
-                total_test += labels.size(0)
-                correct_test += (predicted == labels.cuda()).sum().item()
-
-        print('Accuracy of the network on the test images: {} %'.format(100 * correct_test / total_test))
-    # save trained model
-    torch.save(net.state_dict(), 'saved_model.pt')
-    print('Model saved.')
+    #do sampling method 2
+    train_and_evaluate(trainloader, testloader, 1, 'saved_model_sampling_method2.pt', 2)
